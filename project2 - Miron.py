9ed2c8f882f7e65f20c5916e188be9dd01850a14
mirondanielle <85993454+mirondanielle@users.noreply.github.com>
2021-06-16 05:19:34 -0500

    Add files via upload

---
+++ project2 - Miron.py
@@ -0,0 +1,8 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Name = "Danielle Miron"
+>>> Address = "123 Address Lane Yukon, OK 73099"
+>>> TelephoneNumber = "(123) 456 - 7891"
+>>> print(Name, Address, TelephoneNumber)
+Danielle Miron 123 Address Lane Yukon, OK 73099 (123) 456 - 7891
+>>> 
