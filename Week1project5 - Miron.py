48cb0f57e127048a7fd81cd68e771b104ca4c6fa
mirondanielle <85993454+mirondanielle@users.noreply.github.com>
2021-06-16 05:38:10 -0500

    Add files via upload

---
+++ Week1project5 - Miron.py
@@ -0,0 +1,24 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> base = int(input("Enter the base: "))
+Enter the base: 5
+>>> height = int(input("Enter the height: "))
+Enter the height: 7
+>>> area = .5 * base * height
+>>> print("The area is", area, "square units.")
+The area is 17.5 square units.
+>>> base = int(input("Enter the base: "))
+Enter the base: 6
+>>> height = int(input("Enter the height: "))
+Enter the height: 5
+>>> area = .5 * base * height
+>>> print("The area is", area, "square units.")
+The area is 15.0 square units.
+>>> base = int(input("Enter the base: "))
+Enter the base: 10
+>>> height = int(input("Enter the height: "))
+Enter the height: 10
+>>> area = .5 * base * height
+>>> print("The area is", area, "square units.")
+The area is 50.0 square units.
+>>> 
