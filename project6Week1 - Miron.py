e45ec4ce946d361e8ce9a51a3eff63330b2c3e0d
mirondanielle <85993454+mirondanielle@users.noreply.github.com>
2021-06-16 05:24:35 -0500

    Add files via upload

---
+++ project6Week1 - Miron.py
@@ -0,0 +1,18 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> radius = float(input("Enter the radius: "))
+Enter the radius: 7
+>>> area = 3.14 * radius ** 2
+>>> print ("The area is", area, "square units.")
+The area is 153.86 square units.
+>>> radius = float(input("Enter the radius: "))
+Enter the radius: 6.31
+>>> area = 3.14 * radius ** 2
+>>> print ("The area is", area, "square units.")
+The area is 125.02255399999999 square units.
+>>> radius = float(input("Enter the radius: "))
+Enter the radius: 14.96
+>>> area = 3.14 * radius ** 2
+>>> print ("The area is", area, "square units.")
+The area is 702.7370240000001 square units.
+>>> 
